cd2e19264370a73643b1b1fa7b4a2c9b0f5b3ded
Aleksei Brutov <mjbcaepio@gmail.com>
2023-08-20 13:17:15 +0300

    Домашнее задание №9

--- homework9_1/tasks.py
+++ homework9_1/tasks.py
@@ -1,29 +1,31 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from datetime import datetime
 
 
+class WeatherForecastMeta(type):
+    def __new__(cls, name, bases, attrs):
+        if "create_forecast" in attrs and "date" not in attrs:
+            attrs["date"] = field(default_factory=lambda: datetime.now().strftime("%Y-%m-%d"))
+        return super().__new__(cls, name, bases, attrs)
+
+
 @dataclass
-class WeatherForecast:
-    date: str
+class WeatherForecast(metaclass=WeatherForecastMeta):
     temperature: float
     humidity: int
     description: str
+    date: str
 
     @staticmethod
     def is_cold(temperature):
         return temperature < 10
 
     @classmethod
-    def create_forecast(cls, date, temperature, humidity, description):
-        return cls(date, temperature, humidity, description)
-
-    @staticmethod
-    def todays_date(self):
-        return datetime.now().strftime("%Y-%m-%d")
+    def create_forecast(cls, temperature, humidity, description):
+        return cls(temperature, humidity, description)
 
 
 weather_forecast = WeatherForecast.create_forecast(
-    date=WeatherForecast.todays_date(WeatherForecast),
     temperature=10,
     humidity=20,
     description="Ясно"
